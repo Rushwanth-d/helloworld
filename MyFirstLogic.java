e4187da343c48d8ecd2bb973d0a2065df765cf56
Rushwanth-d <91526189+Rushwanth-d@users.noreply.github.com>
2023-07-21 16:38:07 +0530

    Update MyFirstLogic.java

--- MyFirstLogic.java
+++ MyFirstLogic.java
@@ -4,5 +4,6 @@ class Rushwanth {
         System.out.println("I am Rushwanth");
         System.out.println("I am automation jenkins every mintue i will check ");
         System.out.println("i am lathish i am learning jenkins");
+        System.out.println("i am lathis i am learning jenkins shcedule job");
     }
 }
